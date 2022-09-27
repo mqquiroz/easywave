def335c785d2f552a6e4656252a8aacdbf9afbec
Marco Quiroz <61753596+mqquiroz@users.noreply.github.com>
2022-09-27 20:07:22 -0300

    Add files via upload

--- functions_easywave.py
+++ functions_easywave.py
@@ -109,3 +109,50 @@ def load_surfer(fname, fmt='ascii'):
         raise NotImplementedError(
             "Binary file support is not implemented yet.")
     return x, y, grd, (ny,nx)
+
+def file_netcdf(name,grid,descripcion,dt,ttotal): 
+  # name 'sample.nc', grid:'/content/examples/e2Asean.grd', descripcion: "Simulacion MQ1"
+  import numpy as np
+  import netCDF4 as nc4
+  from scipy.interpolate import griddata
+  from scipy.interpolate import RegularGridInterpolator
+  lon_x, lat_y, grd, (ny,nx) =load_surfer(grid)
+  lon_x=lon_x.reshape(ny,nx)
+  lat_y=lat_y.reshape(ny,nx)
+  grid=grd.reshape(ny,nx)
+  t = np.arange(0,ttotal+dt,dt)
+  pasos = int(ttotal/dt)+1
+
+  f = nc4.Dataset(name,'w', format='NETCDF4') #'w' stands for write
+  f.createDimension('lon',nx)
+  f.createDimension('lat',ny)
+  f.createDimension('time',pasos)
+
+  longitude = f.createVariable('Longitude', 'f4', 'lon')
+  latitude = f.createVariable('Latitude', 'f4', 'lat')  
+  time = f.createVariable('Time', 'i4', 'time')
+  ssh = f.createVariable('SSH', 'f4', ('time', 'lat' , 'lon'))
+
+  longitude[:] = lon_x[0,:]
+  latitude[:]  = lat_y[:,0]
+  time[:] = t
+  lon_lat = [latitude[:],longitude[:]]
+
+  #Add global attributes
+  f.description = descripcion
+  #Add local attributes to variable instances
+  longitude.units = 'degrees'
+  latitude.units = 'degrees'
+  time.units = 'minutes'
+  ssh.units = 'meters'
+
+  for i in range(0,pasos):
+    fileName= 'eWave.2D.{0:05d}.ssh'.format(i*60*dt)
+    lon, lat, h0 = ew2py(fileName)
+    interp = RegularGridInterpolator((lat, lon), h0,bounds_error=False, fill_value=None)
+    data = interp((lat_y,lon_x))#interp((lon_x, lat_y))
+    ssh[i,:,:] = data
+  
+  f.close
+  return f
+
